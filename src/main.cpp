99249ec4776ac84625b808b0acfb74392e88ba04
kapiltadkase <kapiltadkase770@gmail.com>
2026-03-07 19:20:58 +0530

    Writing Records to a Binary File

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,38 @@ using namespace std;
 
 int main(){
     
-    Record r;
-    r.id = 1;
-    r.age = 22;
-    r.isActive = true;
-    r.isDeleted = false;
+   ofstream file("tests/data.db", ios::binary | ios::app);
+   
+   if (!file) {
+    std::cout << "File failed to open\n";
+   }
 
-    cout<<"Size of Record is: "<<sizeof(Record)<<endl;
-    cout<<"Record Struct working"<<endl;
+   Record r1;
+   r1.id = 1;
+   strcpy(r1.name, "Kapil");
+   r1.age = 23;
+   r1.isActive = true;
+   r1.isDeleted = false;
+
+   Record r2;
+   r2.id = 2;
+   strcpy(r2.name,"Varad");
+   r2.age = 21;
+   r2.isActive = true;
+   r2.isDeleted = false;
+
+   Record r3;
+   r3.id  = 3;
+   strcpy(r3.name , "Gautam");
+   r3.age = 21;
+   r3.isActive = true;
+   r3.isDeleted = false;
+   
+
+   file.write(reinterpret_cast<char*>(&r1), sizeof(Record));
+   file.write(reinterpret_cast<char*>(&r2), sizeof(Record));
+   file.write(reinterpret_cast<char*>(&r3), sizeof(Record));
+   file.close();
     
 
     return 0;
