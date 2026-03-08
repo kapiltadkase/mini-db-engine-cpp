352675c85c6971629a2e7fe49fac2e0b51ba9f72
kapiltadkase <kapiltadkase770@gmail.com>
2026-03-08 14:47:49 +0530

    Added Storage class interface for database file operations

---
+++ include/storage.h
@@ -0,0 +1,25 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <string>
+#include "../include/record.h"
+
+class Storage{
+    private:
+    std::string filename;
+
+    public:
+
+    Storage(std::string file);
+    // Just Declaring the function
+    void insertRecord(const Record& record);
+
+    Record readRecord(int index);
+
+    void printAllRecords();
+
+    int getRecordCount();
+
+};
+
+#endif
